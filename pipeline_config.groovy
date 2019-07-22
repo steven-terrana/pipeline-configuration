fe8dee7a5d614be31a949044a63f0207f2d4a756
steven-terrana <steventerrana@gmail.com>
2019-07-22 09:37:57 -0400

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -16,9 +16,10 @@ libraries{
 */
 
 
+branches = ["develop", "jenkins-scm-test"]
 libraries{
   scheduled_builds{
-    branches = ["develop", "jenkins-scm-test"]
+    
     cron_exp = 'H/5 * * * *'
     commit_window = 330
   }
