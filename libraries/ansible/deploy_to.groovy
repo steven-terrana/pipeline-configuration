026c2012dd927473d0abc3439b9d67d2a102dc5f
steven-terrana <steventerrana@gmail.com>
2019-08-12 10:46:24 -0700

    updating

--- libraries/ansible/deploy_to.groovy
+++ libraries/ansible/deploy_to.groovy
@@ -1,5 +1,5 @@
 void call(app_env){
-	stage("Ansible - Deployment"){}
+	stage("Ansible - Deployment"){
 		println "performing a deployment through ansible to ${app_env.long_name}"
 		if(config.customMessage){
 			println config.customMessage
