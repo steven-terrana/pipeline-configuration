d1b7afc01b51017944be870f6e36ccfb04fa475c
steven-terrana <steventerrana@gmail.com>
2019-10-16 12:06:40 -0400

    Update sendMessage.groovy

--- libraries/slack/sendMessage.groovy
+++ libraries/slack/sendMessage.groovy
@@ -1,12 +1,4 @@
-import org.boozallen.plugins.jte.console.TemplateLogger
-@AfterStep({ 
-    TemplateLogger.print """
-    context -> ${context}
-    config -> ${config}
-    """
-    
-    !context.step && config.email_on_complete == true 
-})
+@Notify({ !context.step && config.email_on_complete == true })
 def call(context){
     println "sending a slack notification at the end of the pipeline"
 }
