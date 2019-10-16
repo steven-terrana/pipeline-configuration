da65cc76499ace9493891062f32988fdb8883f02
steven-terrana <steventerrana@gmail.com>
2019-10-16 12:03:59 -0400

    Update sendMessage.groovy

--- libraries/slack/sendMessage.groovy
+++ libraries/slack/sendMessage.groovy
@@ -1,4 +1,10 @@
-@AfterStep({ !context.step && config.email_on_complete == true })
+@AfterStep({ 
+    println """
+    context -> ${context}
+    config -> ${config}
+    """
+    !context.step && config.email_on_complete == true 
+})
 def call(context){
     println "sending a slack notification at the end of the pipeline"
 }
