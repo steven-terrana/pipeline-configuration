93a98434d1d557171add36010897d0fa45604671
Steven Terrana <steventerrana@gmail.com>
2023-03-14 11:40:11 -0400

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -2,13 +2,4 @@ application_environments{
   dev{
     long_name = "Development" 
   }
-} 
-
-libraries{
-  merge = true 
-  sonarqube
-  ansible{
-    customMessage = "Hey from CDF Contributor Summit" 
-  }
 }
-
